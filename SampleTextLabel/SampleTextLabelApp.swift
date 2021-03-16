2d31f91033c76e3984ec96eaf0720caa9b306571
MaskerDog <37284851+MaskerDog@users.noreply.github.com>
2021-03-16 15:38:57 +0900

    Initial Commit

---
+++ SampleTextLabel/SampleTextLabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleTextLabelApp.swift
+//  SampleTextLabel
+//
+//  Created by npc on 2021/03/16.
+//
+
+import SwiftUI
+
+@main
+struct SampleTextLabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
